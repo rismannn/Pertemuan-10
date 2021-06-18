25b879506fcead49f9de5f29dffc5c7c65176682
rismannn <riski21risman@gmail.com>
2021-06-18 22:42:43 +0700

    Pertemuan 10 Risman

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Title title="Catatan Pengeluaran"/>
+  <FormMahasiswa @entri-mahasiswa = "entriMahasiswa($event)"/>
+  <ListMahasiswa v-if="dataMahasiswa.length>0" :dataMahasiswalist="dataMahasiswa"/>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import ListMahasiswa from './components/ListMahasiswa.vue'
+import FormMahasiswa from './components/FormMahasiswa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    ListMahasiswa,
+    FormMahasiswa,
+  },
+  data(){
+    return{
+      dataMahasiswa : [
+        {nim: 'D111811018', nama: 'Riski Risman', kelas: 'IF-6B'},
+        {nim: 'D111811018', nama: 'Risman', kelas: 'IF-6B'},
+        {nim: 'D111811018', nama: 'Rman', kelas: 'IF-6B'}
+      ],
+    }
+  },
+  methods:{
+    entriMahasiswa(event){
+      this.dataMahasiswa.push(event);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
